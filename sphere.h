f781569ea30b67aad572f40278dd54533a7a38a9
MelissaBoateng <67985151+MelissaBoateng@users.noreply.github.com>
2021-05-02 22:29:30 +0300

    Create sphere.h

---
+++ sphere.h
@@ -0,0 +1,10 @@
+#include "cuboid.h"
+
+#include <iostream>
+
+ #include <iomanip>
+ using namespace std;
+
+ class  sphericalBlocks : public cuboid{
+     public sphericalBlocks (double diameter):: cuboid (width)
+ };
